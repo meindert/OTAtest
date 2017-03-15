8bff90ed6241c3b2310e61d725effc5a3e92cc19
meindert <meindert@hoving.co.za>
2017-03-16 00:12:43 +0200

    OLED screen is up

--- testOTA.ino
+++ testOTA.ino
@@ -8,14 +8,23 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+extern "C" {
+#include "user_interface.h"
+}
 // Replace with your network credentials
 const char* ssid = "hoving";
 const char* password = "groningen";
+char buffer[20];
+
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Booting.");
+  Serial.print("Heap: "); Serial.println(system_get_free_heap_size());
+  Serial.print("Boot Vers: "); Serial.println(system_get_boot_version());
+  Serial.print("CPU: "); Serial.println(system_get_cpu_freq());
+  Serial.println();
   ESP.wdtDisable();
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -51,10 +60,30 @@ void setup() {
     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
+  
   ArduinoOTA.begin();
   Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("IP address: "); Serial.println(WiFi.localIP());
+
+ Serial.println("OLED network Acquire Application Started....");
+  //Wire.pins(int sda, int scl), etc
+  Wire.pins(0, 2); //on ESP-01.
+  Wire.begin();
+  StartUp_OLED(); // Init Oled and fire up!
+  Serial.println("OLED Init...");
+  clear_display();
+  sendStrXY(" DANBICKS WIFI ", 0, 1); // 16 Character max per line with font set
+  sendStrXY("   SCANNER     ", 2, 1);
+
+        clear_display(); // Clear OLED
+        IPAddress ip = WiFi.localIP(); // // Convert IP Here
+        String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+        ipStr.toCharArray(buffer, 20);
+        sendStrXY("NETWORK DETAILS", 0, 1);
+        sendStrXY("NET: ", 3, 1);
+       // sendStrXY((ssid), 3, 6);
+        sendStrXY((buffer), 6, 1); // Print IP to yellow part OLED
+  
 }
 
 void loop() {
